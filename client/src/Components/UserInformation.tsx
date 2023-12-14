ed3a4f1caace86bb972d84edc36ca808082adb5e
Erik Reisswig <erikreisswig@gmx.de>
2023-12-14 18:54:11 +0100

    UserInformation und Navigation Design verändert

--- client/src/Components/UserInformation.tsx
+++ client/src/Components/UserInformation.tsx
@@ -3,33 +3,13 @@ import { UserInfoContext } from "../Pages/DashboardLayout";
 
 import "../styles/userInformation.css";
 
+import UserInformationContainer from "./UserInformationContainer";
+
 export default function UserInformation() {
   const userInfo = useContext(UserInfoContext)?.userInfo;
 
   if (userInfo) {
-    return (
-      <section className="user-info card-class">
-        <h2>
-          Willkommen, <span>{userInfo.username} !</span>
-        </h2>
-        <table>
-          <tbody>
-            <tr>
-              <td>Kontostand</td>
-              <td>{userInfo.money} €</td>
-            </tr>
-            <tr>
-              <td>Punkte</td>
-              <td>{userInfo.points}</td>
-            </tr>
-            <tr>
-              <td>Aktives Auto</td>
-              <td>{userInfo.activeCar.name}</td>
-            </tr>
-          </tbody>
-        </table>
-      </section>
-    );
+    return <UserInformationContainer userInfo={userInfo} />;
   }
 
   return <h1>UserInfo Objekt konnte nicht geladen werden</h1>;
